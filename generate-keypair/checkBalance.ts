af2a1884fa7b99ba798175697226f8d0a06dfa8d
Herambh <trunks3107@gmail.com>
2025-04-14 14:34:43 +0530

    Send token from sender to reciever

--- generate-keypair/checkBalance.ts
+++ generate-keypair/checkBalance.ts
@@ -1,14 +1,36 @@
-import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+import { airdropIfRequired } from "@solana-developers/helpers";
+import { Connection, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction } from "@solana/web3.js";
 
 const publicKey = new PublicKey("C3X8LmzXtKTp1CfiCWcrojyTincfbvaFjvsCvFjaYNhC");
 
-if(publicKey){
     const connection = new Connection("https://api.devnet.solana.com", "confirmed");
     const balanceInLamports = await connection.getBalance(publicKey);
     const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
     console.log(
         `💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`,
     );
-} else {
+
     console.log("Public key is not valid!");
-}
+
+
+
+const transaction = new Transaction();
+
+const sendSolInstruction = SystemProgram.transfer({
+  fromPubkey: publicKey,
+  toPubkey: recipient,
+  lamports: LAMPORTS_PER_SOL * 1000,
+});
+
+transaction.add(sendSolInstruction);
+
+const signature = sendAndConfirmTransaction(connection, transaction, [
+    senderKeypair,
+  ]);
+
+  await airdropIfRequired(
+    connection,
+    keypair.publicKey,
+    1 * LAMPORTS_PER_SOL,
+    0.5 * LAMPORTS_PER_SOL,
+  );
